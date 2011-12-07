32ae2017d88582f56b5fa4f2e892c68c9f980c34
potucek <potucek@e1e7a8b5-d3b6-4fb1-a5f8-cf70399aa373>
2011-12-07 00:17:03 +0000

    * validated that garmin build still works after NMEA bug fix

--- spec4/spec4/VersionNo.h
+++ spec4/spec4/VersionNo.h
@@ -1,5 +1,5 @@
-#define FILEVER        1,0,0,1084
-#define PRODUCTVER     1,0,0,1084
-#define STRFILEVER     "1, 0, 0, 1084\0"
-#define STRPRODUCTVER  "1, 0, 0, 1084\0"
-#define STRVERINFO     "LightPilot, Version 1.0.0 Build 1084"
+#define FILEVER        1,0,0,1085
+#define PRODUCTVER     1,0,0,1085
+#define STRFILEVER     "1, 0, 0, 1085\0"
+#define STRPRODUCTVER  "1, 0, 0, 1085\0"
+#define STRVERINFO     "LightPilot, Version 1.0.0 Build 1085"
