ac34e8766c5b305503651934c3aadcad29565e6f
potucek <potucek@e1e7a8b5-d3b6-4fb1-a5f8-cf70399aa373>
2011-12-12 21:53:20 +0000

    And this time it should actually be the "correct" "ac-debug-static" build ...

--- spec4/spec4/VersionNo.h
+++ spec4/spec4/VersionNo.h
@@ -1,5 +1,5 @@
-#define FILEVER        1,0,0,1030
-#define PRODUCTVER     1,0,0,1030
-#define STRFILEVER     "1, 0, 0, 1030\0"
-#define STRPRODUCTVER  "1, 0, 0, 1030\0"
-#define STRVERINFO     "LightPilot, Version 1.0.0 Build 1030"
+#define FILEVER        1,0,0,1032
+#define PRODUCTVER     1,0,0,1032
+#define STRFILEVER     "1, 0, 0, 1032\0"
+#define STRPRODUCTVER  "1, 0, 0, 1032\0"
+#define STRVERINFO     "LightPilot, Version 1.0.0 Build 1032"
