867407644bb9dc13bde8aa5f35753176a2a51c05
potucek <potucek@e1e7a8b5-d3b6-4fb1-a5f8-cf70399aa373>
2011-12-12 21:42:51 +0000

    Hmm ... looks like the build/rebuild clobbers the destination directory, in the process removing the .svn info. Work around this by making a copy of destination directory, building, then copying the new files into the copy and putting that back into place ... or make a backup of the .svn info ...

--- spec4/spec4/VersionNo.h
+++ spec4/spec4/VersionNo.h
@@ -1,5 +1,5 @@
-#define FILEVER        1,0,0,1027
-#define PRODUCTVER     1,0,0,1027
-#define STRFILEVER     "1, 0, 0, 1027\0"
-#define STRPRODUCTVER  "1, 0, 0, 1027\0"
-#define STRVERINFO     "LightPilot, Version 1.0.0 Build 1027"
+#define FILEVER        1,0,0,1030
+#define PRODUCTVER     1,0,0,1030
+#define STRFILEVER     "1, 0, 0, 1030\0"
+#define STRPRODUCTVER  "1, 0, 0, 1030\0"
+#define STRVERINFO     "LightPilot, Version 1.0.0 Build 1030"
