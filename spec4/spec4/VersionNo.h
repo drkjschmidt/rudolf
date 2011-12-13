7dfef539ce1ef09779a81867cca25b5146c24b80
potucek <potucek@e1e7a8b5-d3b6-4fb1-a5f8-cf70399aa373>
2011-12-13 22:02:27 +0000

    another build

--- spec4/spec4/VersionNo.h
+++ spec4/spec4/VersionNo.h
@@ -1,5 +1,5 @@
-#define FILEVER        1,0,0,1036
-#define PRODUCTVER     1,0,0,1036
-#define STRFILEVER     "1, 0, 0, 1036\0"
-#define STRPRODUCTVER  "1, 0, 0, 1036\0"
-#define STRVERINFO     "LightPilot, Version 1.0.0 Build 1036"
+#define FILEVER        1,0,0,1038
+#define PRODUCTVER     1,0,0,1038
+#define STRFILEVER     "1, 0, 0, 1038\0"
+#define STRPRODUCTVER  "1, 0, 0, 1038\0"
+#define STRVERINFO     "LightPilot, Version 1.0.0 Build 1038"
