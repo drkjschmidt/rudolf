bab945df997ad35a90a24bbfe33e1ccc6dab8ba4
potucek <potucek@e1e7a8b5-d3b6-4fb1-a5f8-cf70399aa373>
2011-12-13 21:06:33 +0000

    update for build, this time with fixed filesystem

--- spec4/spec4/VersionNo.h
+++ spec4/spec4/VersionNo.h
@@ -1,5 +1,5 @@
-#define FILEVER        1,0,0,1035
-#define PRODUCTVER     1,0,0,1035
-#define STRFILEVER     "1, 0, 0, 1035\0"
-#define STRPRODUCTVER  "1, 0, 0, 1035\0"
-#define STRVERINFO     "LightPilot, Version 1.0.0 Build 1035"
+#define FILEVER        1,0,0,1036
+#define PRODUCTVER     1,0,0,1036
+#define STRFILEVER     "1, 0, 0, 1036\0"
+#define STRPRODUCTVER  "1, 0, 0, 1036\0"
+#define STRVERINFO     "LightPilot, Version 1.0.0 Build 1036"
