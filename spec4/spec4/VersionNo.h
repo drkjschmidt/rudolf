c0c57278abfc7c53762207d276692f9f92ca1b39
potucek <potucek@e1e7a8b5-d3b6-4fb1-a5f8-cf70399aa373>
2011-12-13 20:50:57 +0000

    Bugfix build 192 for packaging

--- spec4/spec4/VersionNo.h
+++ spec4/spec4/VersionNo.h
@@ -1,5 +1,5 @@
-#define FILEVER        1,0,0,1032
-#define PRODUCTVER     1,0,0,1032
-#define STRFILEVER     "1, 0, 0, 1032\0"
-#define STRPRODUCTVER  "1, 0, 0, 1032\0"
-#define STRVERINFO     "LightPilot, Version 1.0.0 Build 1032"
+#define FILEVER        1,0,0,1035
+#define PRODUCTVER     1,0,0,1035
+#define STRFILEVER     "1, 0, 0, 1035\0"
+#define STRPRODUCTVER  "1, 0, 0, 1035\0"
+#define STRVERINFO     "LightPilot, Version 1.0.0 Build 1035"
