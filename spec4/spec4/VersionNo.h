5aa40e62a2a26e1d4578884b156dc7d5f12e3184
potucek <potucek@e1e7a8b5-d3b6-4fb1-a5f8-cf70399aa373>
2011-12-13 22:58:08 +0000

    Packaging version of 1.1

--- spec4/spec4/VersionNo.h
+++ spec4/spec4/VersionNo.h
@@ -1,5 +1,5 @@
-#define FILEVER        1,1,0,1
-#define PRODUCTVER     1,1,0,1
-#define STRFILEVER     "1, 1, 0, 1\0"
-#define STRPRODUCTVER  "1, 1, 0, 1\0"
-#define STRVERINFO     "LightPilot, Version 1.1.0 Build 1"
+#define FILEVER        1,1,0,2
+#define PRODUCTVER     1,1,0,2
+#define STRFILEVER     "1, 1, 0, 2\0"
+#define STRPRODUCTVER  "1, 1, 0, 2\0"
+#define STRVERINFO     "LightPilot, Version 1.1.0 Build 2"
