34ecc975fcf3b08892838889fc31dd8100afab33
potucek <potucek@e1e7a8b5-d3b6-4fb1-a5f8-cf70399aa373>
2012-01-15 07:11:09 +0000

    build xp commit

--- spec4/spec4/VersionNo.h
+++ spec4/spec4/VersionNo.h
@@ -1,5 +1,5 @@
-#define FILEVER        1,1,0,36
-#define PRODUCTVER     1,1,0,36
-#define STRFILEVER     "1, 1, 0, 36\0"
-#define STRPRODUCTVER  "1, 1, 0, 36\0"
-#define STRVERINFO     "LightPilot, Version 1.1.0 Build 36"
+#define FILEVER        1,1,0,38
+#define PRODUCTVER     1,1,0,38
+#define STRFILEVER     "1, 1, 0, 38\0"
+#define STRPRODUCTVER  "1, 1, 0, 38\0"
+#define STRVERINFO     "LightPilot, Version 1.1.0 Build 38"
